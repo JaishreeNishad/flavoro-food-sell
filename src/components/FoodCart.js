ecf37502fdd11119b42b77fc4cc63a50ac225ede
JaishreeNishad <Jaysh2222@gmail.com>
2024-04-24 12:31:31 +0530

    food data pass using props

--- src/components/FoodCart.js
+++ src/components/FoodCart.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
-
-const FoodCart = () => {
+//child component//
+const FoodCart = ({ id, name, desc, price, rating, img }) => {
   return (
     <div className="font-bold w-[250px] p-5 bg-white flex flex-col rounded-lg">
       <img
-        src="https://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328~exp=1693062928~hmac=53fd9ad496580db41c6ca8066510cd89c6b0a0389de8bb6b875a78a1eda09cb5"
+        src={img}
         alt=""
         className="w-auto h-[130px] cursor-grab transition-all duration-500 hover:scale-110"
       />
       <div className="flex justify-between tex-sm">
-        <h2>Onion Pizza</h2>
-        <span className="text-green-500">₹180</span>
+        <h2>{name}</h2>
+        <span className="text-green-500">₹{price}</span>
       </div>
-      <p className="text-sm font-normal">
-        A delicious pizza topped with fresh onions for a delightful flavor
-        combination.
-      </p>
+      <p className="text-sm font-normal">{desc}</p>
       <div className="flex justify-between">
         <span className="flex justify-center items-center">
           <FaStar className="mr-1 text-yellow-500" />
-          4.5
+          {rating}
         </span>
         <button className="p-1 text-white bg-green-500 text-sm rounded-lg hover:bg-green-600">
           Add to cart
--- src/components/FoodItems.js
+++ src/components/FoodItems.js
@@ -1,12 +1,24 @@
 import React from "react";
 import FoodCart from "./FoodCart";
 import FoodData from "../data/FoodData.js";
-// parent component//
+
 const FoodItems = () => {
   return (
     <div>
+      .
       {FoodData.map((food) => {
-        return <FoodCart key={food.id} />;
+        return (
+          //data pass parent to child using props//
+          <FoodCart
+            key={food.id}
+            id={food.id}
+            name={food.name}
+            price={food.price}
+            img={food.img}
+            desc={food.desc}
+            rating={food.rating}
+          />
+        );
       })}
     </div>
   );
